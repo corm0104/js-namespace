0e2e333d2c9735d190d3e4f98b7ee60532823e5d
corm0104 <32072182+corm0104@users.noreply.github.com>
2017-10-24 15:25:40 -0400

    Create namespace.js

---
+++ namespace.js
@@ -0,0 +1,37 @@
+var CORM0104 = {
+    init: function(){
+        var div= document.createElement("div");
+        document.getElementById("div").className="box";
+        
+        div.textContent="CORM0104";
+        
+        document.getElementById("boxes").appendChild(div);
+        
+        
+        
+        
+        div.addEventListener("click", myFunction);
+        div.addEventListener("mouseover", aFunction);
+        div.addEventListener("mouseout", myThirdFunction);
+        
+        
+        
+        function myFunction(ev) {
+            ev.currentTarget.style.borderColor='blue';
+            ev.currentTarget.style.backgroundColor='yellow';
+            
+        }
+        
+        function aFunction(ev){
+            ev.currentTarget.classList.toggle('highlight');
+        
+        }    
+        
+        function myThirdFunction(ev){
+            ev.currentTarget.classList.toggle('highlight');
+        }  
+        
+    }
+                                  
+                                
+}; 
